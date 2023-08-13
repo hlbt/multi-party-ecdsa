041d3c43be589d600c7bb95a692eea7d26860d3c
hlbt_linux <hlbt_linux@163.com>
2023-08-13 11:10:02 +0800

    Revert "Revert "update so""
    This reverts commit 7678b877c6dddb817eeeab0b5b72f41750a1beab.

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@
 
 pub mod protocols;
 pub mod utilities;
+pub mod gg20_keygen;
 pub mod gg20_android;
 pub mod gg20_sm_client;
 use std::fmt;
@@ -54,61 +55,61 @@ use std::ffi::{CString, CStr};
 use std::slice;
 use surf::Url;
 
-#[no_mangle]
-pub extern fn create_key(address: *const c_char, room: *const c_char, index: c_int, threshold: c_int, number_of_parties: c_int) -> *mut c_char {
-    let c_str_address = unsafe { CStr::from_ptr(address) };
-    let str_address: &str = c_str_address.to_str().unwrap();
-    let string_address: String = str_address.to_owned();
-    let url_address = Url::parse(&string_address).expect("invalid address string");
+// #[no_mangle]
+// pub extern fn create_key(address: *const c_char, room: *const c_char, index: c_int, threshold: c_int, number_of_parties: c_int) -> *mut c_char {
+//     let c_str_address = unsafe { CStr::from_ptr(address) };
+//     let str_address: &str = c_str_address.to_str().unwrap();
+//     let string_address: String = str_address.to_owned();
+//     let url_address = Url::parse(&string_address).expect("invalid address string");
 
-    let c_str_room = unsafe { CStr::from_ptr(room) };
-    let str_room: &str = c_str_room.to_str().unwrap();
-    let string_room: String = str_room.to_owned();
-
-
-    let u16_index: u16 = index as u16;
-
-    let u16_threshold: u16 = threshold as u16;
-
-    let u16_number_of_parties: u16 = number_of_parties as u16;
-
-    let task = crate::gg20_android::create_key_async(url_address, string_room, u16_index, u16_threshold, u16_number_of_parties);
-    let result = tokio::runtime::Runtime::new().unwrap().block_on(task);
+//     let c_str_room = unsafe { CStr::from_ptr(room) };
+//     let str_room: &str = c_str_room.to_str().unwrap();
+//     let string_room: String = str_room.to_owned();
 
 
-    CString::new(result.unwrap()).unwrap().into_raw()
-}
+//     let u16_index: u16 = index as u16;
 
-#[no_mangle]
-pub extern fn sign_data(address: *const c_char, room: *const c_char, parties: *const c_int, data_to_sign: *const c_char, local_share: *const c_char) -> *mut c_char {
-    let c_str_address = unsafe { CStr::from_ptr(address) };
-    let str_address: &str = c_str_address.to_str().unwrap();
-    let string_address: String = str_address.to_owned();
-    let url_address = Url::parse(&string_address).expect("invalid address string");
+//     let u16_threshold: u16 = threshold as u16;
 
-    let c_str_room = unsafe { CStr::from_ptr(room) };
-    let str_room: &str = c_str_room.to_str().unwrap();
-    let string_room: String = str_room.to_owned();
+//     let u16_number_of_parties: u16 = number_of_parties as u16;
 
-    let c_int_parties: &[c_int] = unsafe {
-        let len = 2;
-        slice::from_raw_parts(parties, len)
-    };
-    let u16_vec_parties: Vec<u16> = c_int_parties.iter().map(|&c| c as u16).collect();
+//     let task = crate::gg20_android::create_key_async(url_address, string_room, u16_index, u16_threshold, u16_number_of_parties);
+//     let result = tokio::runtime::Runtime::new().unwrap().block_on(task);
 
 
-    let c_str_data_to_sign = unsafe { CStr::from_ptr(data_to_sign) };
-    let str_data_to_sign: &str = c_str_data_to_sign.to_str().unwrap();
-    let string_data_to_sign: String = str_data_to_sign.to_owned();
+//     CString::new(result.unwrap()).unwrap().into_raw()
+// }
 
-    let c_str_local_share = unsafe { CStr::from_ptr(local_share) };
-    let str_local_share: &str = c_str_local_share.to_str().unwrap();
-    let string_local_share: String = str_local_share.to_owned();
+// #[no_mangle]
+// pub extern fn sign_data(address: *const c_char, room: *const c_char, parties: *const c_int, data_to_sign: *const c_char, local_share: *const c_char) -> *mut c_char {
+//     let c_str_address = unsafe { CStr::from_ptr(address) };
+//     let str_address: &str = c_str_address.to_str().unwrap();
+//     let string_address: String = str_address.to_owned();
+//     let url_address = Url::parse(&string_address).expect("invalid address string");
 
-    let task = crate::gg20_android::sign_data_async(url_address, string_room, u16_vec_parties, string_data_to_sign, string_local_share);
-    let result = tokio::runtime::Runtime::new().unwrap().block_on(task);
-    CString::new(result.unwrap()).unwrap().into_raw()
-}
+//     let c_str_room = unsafe { CStr::from_ptr(room) };
+//     let str_room: &str = c_str_room.to_str().unwrap();
+//     let string_room: String = str_room.to_owned();
+
+//     let c_int_parties: &[c_int] = unsafe {
+//         let len = 2;
+//         slice::from_raw_parts(parties, len)
+//     };
+//     let u16_vec_parties: Vec<u16> = c_int_parties.iter().map(|&c| c as u16).collect();
+
+
+//     let c_str_data_to_sign = unsafe { CStr::from_ptr(data_to_sign) };
+//     let str_data_to_sign: &str = c_str_data_to_sign.to_str().unwrap();
+//     let string_data_to_sign: String = str_data_to_sign.to_owned();
+
+//     let c_str_local_share = unsafe { CStr::from_ptr(local_share) };
+//     let str_local_share: &str = c_str_local_share.to_str().unwrap();
+//     let string_local_share: String = str_local_share.to_owned();
+
+//     let task = crate::gg20_android::sign_data_async(url_address, string_room, u16_vec_parties, string_data_to_sign, string_local_share);
+//     let result = tokio::runtime::Runtime::new().unwrap().block_on(task);
+//     CString::new(result.unwrap()).unwrap().into_raw()
+// }
 
 #[no_mangle]
 pub extern fn rust_free(s: *mut c_char) {
