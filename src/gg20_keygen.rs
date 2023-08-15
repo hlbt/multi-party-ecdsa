be43966d14d9e997e51bd569da34fe17df6966d7
hlbt_linux <hlbt_linux@163.com>
2023-08-15 20:56:50 +0800

    add refresh

--- src/gg20_keygen.rs
+++ src/gg20_keygen.rs
@@ -18,14 +18,11 @@ pub fn free_keygen(keygen_ptr: *mut Keygen) {
 }
 
 pub fn handle_incoming(keygen_ptr: *mut Keygen, msg_json: String) {
-    debug!("gg20_keygen handle_incoming start");
     let keygent = unsafe {
         assert!(!keygen_ptr.is_null());
         &mut *keygen_ptr
     };
-    debug!("gg20_keygen handle_incoming serde_json start");
     let msg = serde_json::from_str::<Msg<ProtocolMessage>>(&msg_json).expect("deserialize message");
-    debug!("gg20_keygen eygent.handle_incoming start");
     keygent.handle_incoming(msg).unwrap();
 }
 
